954b0fa72a494c9993ab1980b6cfd82dd7285712
leejongsuu <lik9819@naver.com>
2024-07-18 16:03:51 +0900

    problem

--- src/test/java/com/example/concurrent/service/StockServiceTest.java
+++ src/test/java/com/example/concurrent/service/StockServiceTest.java
@@ -9,6 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class StockServiceTest {
 
@@ -34,7 +40,30 @@ class StockServiceTest {
 
         // 100 - 1 = 99
         Stock stock = stockRepository.findById(1L).orElseThrow();
-        Assertions.assertEquals(99L, stock.getQuantity());
+        assertEquals(99L, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decreaseStock(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 - (1 * 100) = 0
+        assertEquals(0, stock.getQuantity());
     }
 
 }
